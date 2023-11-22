bfc36cc4c2f83c67748e097bee8c2507618a5cf0
IcyChlorine <icy_chlorine@pku.edu.cn>
2023-11-22 19:16:32 +0800

    fix shaders

--- GLProjFrame/src/shaders/vertex.glsl
+++ GLProjFrame/src/shaders/vertex.glsl
@@ -1,7 +1,7 @@
 #version 460 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
-layout (location = 2) varying in vec2 aTexCoord;
+layout (location = 2) in vec2 aTexCoord;
 
 out vec3 ourColor;
 out vec2 TexCoord;

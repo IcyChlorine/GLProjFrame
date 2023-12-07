57957c01d4ca0c5c69365762a1cc413043c704b2
IcyChlorine <icy_chlorine@pku.edu.cn>
2023-12-07 13:31:44 +0800

    cleanup codes

--- GLProjFrame/src/Application.cpp
+++ GLProjFrame/src/Application.cpp
@@ -63,7 +63,6 @@ Application::Application()
 Application::~Application()
 {
 	glfwTerminate();
-	delete shader;
 	delete inputManager;
 	delete camera;
 }
--- GLProjFrame/src/Application.h
+++ GLProjFrame/src/Application.h
@@ -21,35 +21,9 @@ private:
 	bool line_mode{ false };
 protected:
 	Window* window{ nullptr };
-	Shader* shader{ nullptr };
 	InputManager* inputManager{ nullptr };
 	GameCamera* camera{ nullptr };
-	Texture* texture{ nullptr };
 	//stringstream consoleOutput;
-
-
-	//preparing vertex data and convey them to GPU
-	float vertices[32] = {
-		// positions          // colors           // texture coords
-		0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-		0.5f, -0.5f, -0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-		-0.5f, -0.5f, -0.5f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-		-0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-	};
-	unsigned int indices[6] = {  // note that we start from 0!
-		0, 2, 1,
-		0, 3, 2
-	};
-	float texCoords[6] = {
-		0.0f, 0.0f,  // lower-left corner  
-		1.0f, 0.0f,  // lower-right corner
-		0.5f, 1.0f   // top-center corner
-	};
-	//generate VBO/EBO presets via VAO
-	unsigned int VAO;
-	unsigned int VBO;
-	unsigned int EBO;
-
 	
 public:
 	Application();
